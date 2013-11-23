3c42ceac2d41266c529202db5ce5a2002c7f35bd
Pradeep CR <speedcrp@gmail.com>
2013-11-24 00:00:30 +0530

    added tabs and linked it with pages
    tabs now listen page changes and pages now listen tab changes.

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -17,51 +17,75 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        var supportsOrientationChange = "onorientationchange" in window,
-        orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+		// Application Constructor
+		initialize: function() {
+			this.bindEvents();
+		},
+		// Bind Event Listeners
+		//
+		// Bind any events that are required on startup. Common events are:
+		// 'load', 'deviceready', 'offline', and 'online'.
+		bindEvents: function() {
+			document.addEventListener('deviceready', this.onDeviceReady, false);
+			var supportsOrientationChange = "onorientationchange" in window,
+			orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
 
-	    window.addEventListener(orientationEvent, this.setScreenBounds, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-    	app.setScreenBounds();
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);        
-        console.log('Received Event: ' + id);
-        
-        var mySwiper = new Swiper('.swiper-container',{
-            mode:'horizontal',
-            loop: false
-            //etc..
-          }); 
-    },
-    
-    setScreenBounds: function(){
-    	var availableWidth = window.screen.availWidth;
-    	var availableHeight = window.screen.availHeight;
-    	
-    	var height = availableHeight > availableWidth ? availableHeight : availableWidth;
-    	
-    	if(window.orientation != 0){
-    		var height = availableHeight > availableWidth ? availableWidth : availableHeight;
-    	}
-    	
-    	$(".swiper-container").css("height", height+"px").trigger("true");
-    }    
+			window.addEventListener(orientationEvent, this.setScreenBounds, false);
+		},
+		// deviceready Event Handler
+		//
+		// The scope of 'this' is the event. In order to call the 'receivedEvent'
+		// function, we must explicity call 'app.receivedEvent(...);'
+		onDeviceReady: function() {
+			app.setScreenBounds();
+			app.receivedEvent('deviceready');
+		},
+		// Update DOM on a Received Event
+		receivedEvent: function(id) {
+
+			myTabs = $('.tabs').swiper({
+				mode:'horizontal',
+				slidesPerView : 1.5,
+				initialSlide : 2,
+				offsetSlidesBefore : 0.25,
+				shortSwipes : true,
+				onSlideChangeEnd: app.onTabChanged,
+				loop: false
+			}); 
+
+			mySwiper = $('.swiper-container').swiper({
+				mode:'horizontal',
+				slidesPerView : 1.5,
+				initialSlide : 2,
+				offsetSlidesBefore : 0.25,
+				shortSwipes : true,
+				pagination:document.getElementById("pagination"),
+				createPagination : true,
+				paginationClickable: true,
+				onSlideChangeEnd: app.onSlideChanged,
+				loop: false
+			}); 
+		},
+
+		onSlideChanged : function(e){
+			myTabs.swipeTo(mySwiper.activeIndex, 0, false);
+		},
+		
+		onTabChanged : function(e){
+			mySwiper.swipeTo(myTabs.activeIndex, 0, false);
+		},
+
+		setScreenBounds: function(){
+			var availableWidth = window.screen.availWidth;
+			var availableHeight = window.screen.availHeight;
+
+			var height = availableHeight > availableWidth ? availableHeight : availableWidth;
+
+			if(window.orientation != 0){
+				var height = availableHeight > availableWidth ? availableWidth : availableHeight;
+			}
+
+			$(".tabs").css("height", 50+"px").trigger("true");
+			$(".swiper-container").css("height", (height-52)+"px").trigger("true");
+		}    
 };
